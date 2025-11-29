b55163bd3b0f5e644cfeea3e2eb885e858cfe212
angelolima <angelomiguellima@gmail.com>
2025-11-29 22:30:25 +0100

    Fix AltarBlock: correct ghost destruction behavior
    - Added blocksMotion = true in constructor
    - Fixed sprite position to (0,0,0)
    - When ghost is destroyed: altar no longer blocks, sprite is removed,
      8 RubbleSprite particles are spawned with altar color
    - Fixed sound: altar.play() for normal ghosts, bosskill.play() for boss

--- src/level/block/AltarBlock.ts
+++ src/level/block/AltarBlock.ts
@@ -1,6 +1,7 @@
 import { Art } from "../../Art";
 import { Sound } from "../../Sound";
 import { Sprite } from "../../gui/Sprite";
+import { RubbleSprite } from "../../gui/RubbleSprite";
 import type { Entity } from "../../entities/Entity";
 import { GhostEntity } from "../../entities/GhostEntity";
 import { GhostBossEntity } from "../../entities/GhostBossEntity";
@@ -13,20 +14,30 @@ export class AltarBlock extends Block {
 
   constructor() {
     super();
-    this.altarSprite = new Sprite(0.5, 0, 0.5, 16 + 4, Art.getCol(0xe2ffe4));
+    this.blocksMotion = true;
+    this.altarSprite = new Sprite(0, 0, 0, 16 + 4, Art.getCol(0xe2ffe4));
     this.sprites.push(this.altarSprite);
   }
 
   addEntity(entity: Entity): void {
     super.addEntity(entity);
     if (!this.filled && (entity instanceof GhostEntity || entity instanceof GhostBossEntity)) {
-      Sound.altar.play();
       entity.remove();
-      this.altarSprite.col = Art.getCol(0x00ff00);
       this.filled = true;
+      this.blocksMotion = false;
+      this.altarSprite.removed = true;
+
+      for (let i = 0; i < 8; i++) {
+        const rubble = new RubbleSprite();
+        rubble.col = this.altarSprite.col;
+        this.addSprite(rubble);
+      }
+
       if (entity instanceof GhostBossEntity) {
-        Sound.bosskill.play();
         this.level!.addEntity(new KeyEntity(this.x, this.y));
+        Sound.bosskill.play();
+      } else {
+        Sound.altar.play();
       }
     }
   }
