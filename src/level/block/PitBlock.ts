a8a47ca20b2e2d725a7c4d00b8b912691384fc9c
angelolima <angelomiguellima@gmail.com>
2025-11-29 22:29:58 +0100

    Fix PitBlock: add boulder sprite instead of changing floor color
    When a boulder fills a pit, now correctly adds a sprite of the boulder
    on top instead of changing the floor texture and color to purple.
    Matches the original Java implementation.

--- src/level/block/PitBlock.ts
+++ src/level/block/PitBlock.ts
@@ -1,7 +1,7 @@
-import { Art } from "../../Art";
 import { Sound } from "../../Sound";
 import type { Entity } from "../../entities/Entity";
 import { BoulderEntity } from "../../entities/BoulderEntity";
+import { Sprite } from "../../gui/Sprite";
 import { Block } from "./Block";
 
 export class PitBlock extends Block {
@@ -11,18 +11,16 @@ export class PitBlock extends Block {
     super();
     this.floorTex = 1;
     this.blocksMotion = true;
-    this.floorCol = Art.getCol(0x804040);
   }
 
   addEntity(entity: Entity): void {
     super.addEntity(entity);
     if (!this.filled && entity instanceof BoulderEntity) {
-      Sound.thud.play();
+      entity.remove();
       this.filled = true;
       this.blocksMotion = false;
-      this.floorTex = 0;
-      this.floorCol = Art.getCol(0x8a6496);
-      entity.remove();
+      this.addSprite(new Sprite(0, 0, 0, 8 + 2, BoulderEntity.COLOR));
+      Sound.thud.play();
     }
   }
 
